2f4fcb723a4426d23bd8092f255e98be0b26acc7
EricWong <a2370889@163.com>
2018-05-11 08:55:03 +0800

    判断回文链表 采用递归回溯 另一种思路：快慢双指针

---
+++ Palindrome.java
@@ -0,0 +1,36 @@
+package Cracking_Coding_Interview;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Palindrome {
+	List list = new ArrayList();
+	volatile int count = 0;
+	volatile boolean flag = true;
+
+	public boolean isPalindrome(ListNode pHead) {
+		if (pHead == null)
+			return true;
+		list.add(pHead.val);
+		flag = isPalindrome(pHead.next);
+		if (!list.get(count).equals(pHead.val)) {
+			return false;
+		}
+		count++;
+		return flag;
+	}
+
+	public static void main(String[] args) {
+		ListNode node = new ListNode(1);
+		ListNode node1 = new ListNode(2);
+		ListNode node2 = new ListNode(3);
+		ListNode node3 = new ListNode(2);
+		ListNode node4 = new ListNode(3);
+		node.next = node1;
+		node1.next = node2;
+		node2.next = node3;
+		node3.next = node4;
+		Palindrome palindrome = new Palindrome();
+		System.out.println(palindrome.isPalindrome(node));
+	}
+}
